dc6e3f9d17d92f95f8b3232ac7e9a47b4c72bc5e
dusrpsap <alex.gomes@inmetrics.com.br>
2020-10-15 18:39:46 -0300

    docker-compose elastic stack

--- routes/api/data.js
+++ routes/api/data.js
@@ -26,98 +26,96 @@ router.get('/earthquakes', async function (req, res) {
     //     );
     // }
 
+    try {
+        
+        console.log('Getting Data From Host')
 
-            console.log('Getting Data From Host')
+        const EARTHQUAKES = await get(`${URL}`,{
+            headers: {
+                'Content-Type': [
+                    'application/json',  
+                    'charset=utf-8' 
+                ]
+            }
+        });
 
-            const EARTHQUAKES = await get(`${URL}`,{
-                headers: {
-                    'Content-Type': [
-                        'application/json',  
-                        'charset=utf-8' 
-                    ]
-                }
-            });
+        console.log('Data Received!')
 
-            console.log('Data Received!')
+        results = EARTHQUAKES.data.features
 
-            results = EARTHQUAKES.data.features
+        console.log('Indexing Data...')
 
-            console.log('Indexing Data...')
+        results.map(async results => (
+            earthquakeObject = {
+                place: results.properties.place,
+                time: results.properties.time,
+                tiamp: results.properties.time,
+                updmestated: results.properties.updated,
+                tz: results.properties.tz,
+                url: results.properties.url,
+                detail: results.properties.detail,
+                felt: results.properties.felt,
+                cdi: results.properties.cdi,
+                alert: results.properties.alert,
+                status: results.properties.status,
+                tsunami: results.properties.tsunami,
+                sig: results.properties.sig,
+                net: results.properties.net,
+                code: results.properties.code,
+                sources: results.properties.sources,
+                nst: results.properties.nst,
+                dmin: results.properties.dmin,
+                rms: results.properties.rms,
+                mag: results.properties.mag,
+                magType: results.properties.magType,
+                type: results.properties.type,
+                latitude: results.geometry.coordinates[0],
+                longitude: results.geometry.coordinates[1],
+                location:
+                    { 
+                        lat: results.geometry.coordinates[1],
+                        lon: results.geometry.coordinates[0],
+                    },
+                depth: results.geometry.coordinates[2]
+            }                
+        ));
+        
+        // Implementar a atualizacao do Index ELK
 
-            results.map(async results => (
-                earthquakeObject = {
-                    place: results.properties.place,
-                    time: results.properties.time,
-                    tiamp: results.properties.time,
-                    updmestated: results.properties.updated,
-                    tz: results.properties.tz,
-                    url: results.properties.url,
-                    detail: results.properties.detail,
-                    felt: results.properties.felt,
-                    cdi: results.properties.cdi,
-                    alert: results.properties.alert,
-                    status: results.properties.status,
-                    tsunami: results.properties.tsunami,
-                    sig: results.properties.sig,
-                    net: results.properties.net,
-                    code: results.properties.code,
-                    sources: results.properties.sources,
-                    nst: results.properties.nst,
-                    dmin: results.properties.dmin,
-                    rms: results.properties.rms,
-                    mag: results.properties.mag,
-                    magType: results.properties.magType,
-                    type: results.properties.type,
-                    latitude: results.geometry.coordinates[0],
-                    longitude: results.geometry.coordinates[1],
-                    location:
-                        { 
-                            lat: results.geometry.coordinates[1],
-                            lon: results.geometry.coordinates[0],
-                        },
-                    depth: results.geometry.coordinates[2]
-                }                
-            ));
-            
-            // Implementar a atualizacao do Index ELK
+        // client.index({  
+        //     index: 'earthquakes',
+        //     id: results.id,
+        //     type: 'devops',
+        //     body: earthquakeObject
+        // },function(err,resp,status) {
+        //     console.log(resp);
+        // });
 
-            // client.index({  
-            //     index: 'earthquakes',
-            //     id: results.id,
-            //     type: 'devops',
-            //     body: earthquakeObject
-            // },function(err,resp,status) {
-            //     console.log(resp);
-            // });
+        // client.bulk({  
+        //     index: 'earthquakes',
+        //     type: 'devops',
+        //     body: earthquakeObject
+        //   },function(err,resp,status) {
+        //     console.log(resp);
+        // });
 
-            client.bulk({  
-                index: 'earthquakes',
-                type: 'devops',
-                body: earthquakeObject
-              },function(err,resp,status) {
-                console.log(resp);
-            });
+        //console.log(results)
+        res.json(results)
+        
 
-            //console.log(results)
-            res.json(results)
-            
-
-            if (EARTHQUAKES.data.length) {
-                indexAllDocs();
-            } else {
-                console.log('All Data Has Been Indexed!');
-            };
-
-            
-        } catch (err) {
-            console.log(err)
+        if (EARTHQUAKES.data.length) {
+            //indexAllDocs();
+            console.log('More Data!');
+        } else {
+            console.log('All Data Has Been Indexed!');
         };
 
-        console.log('Preparing For The Next Data Check...');
-    }
+        
+    } catch (err) {
+        console.log(err)
+    };
 
-    //pingElasticsearch().catch(console.log)
-    indexAllDocs().catch(console.log)
+    console.log('Preparing For The Next Data Check...');
 });
  
 module.exports = router;
